174c6b0fdcb1d4b227d12847e5b74399ae536a4b
Mohd Alquama <mohdalquama1@gmail.com>
2025-06-21 01:10:45 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Required to emulate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 });
