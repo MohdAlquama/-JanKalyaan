daf46be2e9e6cc0ba10ea6e15d023bc8c4fbebb1
Mohd Alquama <mohdalquama1@gmail.com>
2025-06-21 01:11:02 +0530

    update main file code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SchemeDetail from '@/components/Scheme/SchemeDetail';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+  <Router>
+      <Routes>
+        <Route path="/" element={< App />} />
+        <Route path="/about" element={<h1> hello </h1>} />
+                <Route path="/scheme/:id" element={<SchemeDetail/>} />
+      </Routes>
+    </Router>
   </StrictMode>
 );
+
+
+// src/App.jsx
+
+
+
+export default App;
